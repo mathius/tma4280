90b5d6508ac013bd368ef7c24a33b0c3d15ba654
Martin Ukrop <mukrop@mail.muni.cz>
2014-02-07 23:05:51 +0100

    ps04, serial WAS working

--- ps04/algebra.h
+++ ps04/algebra.h
@@ -1,4 +1,28 @@
 #ifndef ALGEBRA_H
 #define ALGEBRA_H
 
+#include <cstdlib>
+#include <cmath>
+
+typedef struct Vector {
+    int lengthGlobal;
+    int lengthLocal;
+    int offsetLocal;
+    int commSize;
+    int commRank;
+    double* data;
+    Vector(int length, double* existingData = NULL);
+    ~Vector();
+} Vector;
+
+void initialize(int argc, char** argv);
+void finalize();
+
+int getWorldRank();
+int getWorldSize();
+
+//! \brief Get current wall-clock time
+//! \return The current wall time in seconds
+double wallTime();
+
 #endif // ALGEBRA_H
--- ps04/ps04.cpp
+++ ps04/ps04.cpp
@@ -1,173 +1,62 @@
 #include <iostream>
-#include <sstream>
-#include <cstdlib>
-#include <sys/time.h>
-#include <mpi.h>
+#include "algebra.h"
 
 using namespace std;
 
-#define N 5
+double doIteration(int vectorLength) {
 
-typedef struct Vector {
-    int globalLength;
-    int localLength;
-    int localOffset;
-    int commSize;
-    int commRank;
-    double* data;
+    double* receiveData = NULL;
 
-    Vector(int length) {
-        globalLength = length;
-        localLength = length;
-        localOffset = 0;
-        commRank = 0;
-        commSize = 1;
-        data = new double[localLength];
-        for (int i = 0; i < localLength; i++) {
-            data[i] = i;
+    if (getWorldRank() == 0) {
+        // generate vector
+        double* tmpVector = new double[vectorLength];
+        for (int i = 0; i < vectorLength; i++) {
+            tmpVector[i] = (double) 1 / ((i+1)*(i+1));
         }
 
-    }
-    Vector(int length, int size, int rank) {
-        globalLength = length;
-        commSize = size;
-        commRank = rank;
-        localLength = globalLength / commSize;
-        if (commRank < globalLength % commSize) {
-            localLength++;
+#ifdef HAVE_MPI
+        // spread vector among processes
+        for (int i = 1; i < getWorldSize(); i++) {
+            MPI::COMM_WORLD.Send(tmpVector[getOffset(i)], getLength(i), MPI::DOUBLE, i, 100);
         }
-        localOffset = commRank * (globalLength / commSize) + min(globalLength % commSize, commRank);
-        data = new double[localLength];
-        for (int i = 0; i < localLength; i++) {
-            data[i] = i;
-        }
+#endif
+        receiveData = tmpVector;
     }
-    ~Vector() {
-        delete[] data;
-    }
-} Vector;
 
-typedef struct Matrix {
-    int nRows;
-    int nColumns;
-    int localNColumns;
-    int localColOffset;
-    double* data;
-    int commSize;
-    int commRank;
-    Matrix(int rows, int columns, int size, int rank) {
-        commSize = size;
-        commRank = rank;
-        nRows = rows;
-        nColumns = columns;
-        localNColumns = nColumns / commSize;
-        if (commRank < nColumns % commSize) {
-            localNColumns++;
-        }
-        localColOffset = commRank * (nColumns / commSize) + min(nColumns % commSize, commRank);
-        data = new double[localNColumns*nRows];
-        for (int i = 0; i < localNColumns*nRows; i++) {
-            data[i] = i;
-        }
-    }
-    ~Matrix() {
-        delete[] data;
-    }
-} Matrix;
+    // recieve vector parts
+#ifdef HAVE_MPI
+    //MPI_recieve (from 0 to  not a keyword. It's an identifier defined in some standard headers. You can inreceiveData)
+#endif
+    Vector* localVector = new Vector(vectorLength, receiveData);
+    double sum = 0;
 
-double wallTime ()
-{
-    struct timeval tmpTime; gettimeofday(&tmpTime,NULL);
-    return tmpTime.tv_sec + tmpTime.tv_usec/1.0e6;
+    // compute sum (starting from smaller numbers)
+    for (int i = localVector->lengthLocal-1; i >= 0; i--) {
+        sum += localVector->data[i];
+    }
+
+    // reduce if needed
+#ifdef HAVE_MPI
+    double tmpSum = sum;
+    //MPI_reduce (to 0 from tmpSum to sum);
+#endif
+
+    delete[] receiveData;
+    delete localVector;
+    return sum;
 }
 
 int main(int argc, char** argv) {
-    srand(wallTime());
-    int rank, size;
+    initialize(argc, argv);
 
-    MPI::Init(argc, argv);
-    MPI::COMM_WORLD.Set_errhandler(MPI::ERRORS_THROW_EXCEPTIONS);
-    size = MPI::COMM_WORLD.Get_size();
-    rank = MPI::COMM_WORLD.Get_rank();
+    double pi = 4 * atan(1);
+    double preciseSum = pi*pi/6;
 
-    if (argc < 2) {
-        cerr << "error: Need one numerical argument." << endl;
-        return EXIT_FAILURE;
+    for (unsigned int i = (int) pow(2,3); i < pow(2,15); i *= 2) {
+        double computedSum = doIteration(i);
+        cout << "n: " << i << "\tdiff: " << preciseSum - computedSum << endl;
     }
 
-    double gamma;
-    stringstream ss(argv[1]);
-    ss >> gamma;
-    if (ss.fail()) {
-        cerr << "error: Could not interpret argument '" << string(argv[1]) << "' as number." << endl;
-        return EXIT_FAILURE;
-    }
-
-    Vector* a = new Vector(N, size, rank);
-    Vector* b = new Vector(N, size, rank);
-    Vector* x = new Vector(N, size, rank);
-    Vector* y = new Vector(N);
-    Vector* yTemp = new Vector(N);
-    Matrix* A = new Matrix(N, N, size, rank);
-
-    for (int i = 0; i < b->localLength; i++) {
-        x->data[i] = a->data[i] + gamma * b->data[i];
-        //result += x->data[i] * x->data[i];
-        //cout << "rank: " << rank << ", result: " << x->data[i] << endl;
-    }
-    //mpiTmp = result;
-    //MPI::COMM_WORLD.Allreduce(&mpiTmp, &result, 1, MPI_DOUBLE, MPI_SUM);
-
-    for (int row = 0; row < A->nRows; row++) {
-        yTemp->data[row] = 0;
-        for (int i = 0; i < b->localLength; i++) {
-            yTemp->data[row] += A->data[row + i * A->nRows] * b->data[i];
-            //cout << "### rank: " << rank << " " << A->data[row + i * A->nRows] * b->data[i] << endl;
-        }
-        //cout << "rank: " << rank << ", yTemp[" << row << "]: " << yTemp->data[row] << endl;
-    }
-    for (int i = 0; i < yTemp->globalLength; i++) {
-        y->data[i] = 0;
-        MPI::COMM_WORLD.Allreduce(&(yTemp->data[i]), &(y->data[i]), 1, MPI::DOUBLE, MPI::SUM);
-        //cout << "rank: " << rank << ", y[" << i << "]: " << y->data[i] << endl;
-    }
-
-    double alpha = 0;
-    double alphaTmp = 0;
-    for (int i = 0; i < x->localLength; i++) {
-        //cout << "rank: " << rank << " alpha[" << i << "]: " << x->data[i] * (a->data[i] + y->data[i + x->localOffset]) << endl;
-        alphaTmp += x->data[i] * (a->data[i] + y->data[i + x->localOffset]);
-    }
-    MPI::COMM_WORLD.Reduce(&alphaTmp, &alpha, 1, MPI::DOUBLE, MPI::SUM, 0);
-
-    if (rank == 0) {
-        cout << "result: " << alpha << endl;
-    }
-
-    MPI_Finalize();
+    finalize();
     return EXIT_SUCCESS;
 }
-
-/*
-
-a,b: 0 1 2 0 1
-x = a + gamma*b = 0 1.1 2.2 0 1.1
-A =
-0 5 10 0 5
-1 6 11 1 6
-2 7 12 2 7
-3 8 13 3 8
-4 9 14 4 9
-
-Ab =
-(0+5+20)+(0+5) = 30
-(0+6+22)+(0+6) = 34
-38
-42
-46
-
-y = a+Ab = 30 35 40 42 47
-
-xTy = 0 + 1.1*35 + 2.2*40 + 0 + 1.1*47
-
- */
